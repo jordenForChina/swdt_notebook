1fe66df0b35c35c4f9c15a71022d0daa1a7d4711
jorden <378015802@qq.com>
2016-11-02 23:04:49 +0800

    angular

---
+++ angular.mm
@@ -0,0 +1,85 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#ff0033" CREATED="1478096411853" ID="ID_1564371876" MODIFIED="1478098424879" STYLE="bubble" TEXT="angular">
+<font NAME="SansSerif" SIZE="11"/>
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<node CREATED="1478098296897" HGAP="8" ID="ID_333126496" MODIFIED="1478098755073" POSITION="right" TEXT="route" VSHIFT="-120">
+<node CREATED="1478096411853" ID="ID_510135416" MODIFIED="1478098756579" TEXT="ng-route">
+<node COLOR="#0000cc" CREATED="1478096719762" FOLDED="true" ID="ID_909050368" MODIFIED="1478098865382" TEXT="&#x57fa;&#x672c;">
+<node CREATED="1478096809011" HGAP="25" ID="ID_109374302" MODIFIED="1478097289664" TEXT="ng-app=&quot;app&quot;" VSHIFT="-18">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1478096824182" FOLDED="true" ID="ID_711633480" MODIFIED="1478097752430" TEXT="&#x5bfc;&#x5165;js&#x6587;&#x4ef6;">
+<icon BUILTIN="full-2"/>
+<node CREATED="1478096854438" HGAP="38" ID="ID_683132533" MODIFIED="1478096935910" TEXT="angular.js" VSHIFT="-11"/>
+<node CREATED="1478096879390" HGAP="32" ID="ID_1205756080" MODIFIED="1478096937782" TEXT="angular.route.js" VSHIFT="4"/>
+</node>
+<node CREATED="1478098593741" HGAP="24" ID="ID_651738196" MODIFIED="1478098861848" VSHIFT="21">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;div ng-view&gt;&lt;/div&gt;
+    </p>
+    <p>
+      <font color="#ff0000">&lt;a href=&quot;#/home&quot;&gt;&lt;/a&gt;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="clanbomber"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1478097091336" HGAP="21" ID="ID_681552823" MODIFIED="1478098659463" TEXT="var app = angular.module(&apos;app&apos;,[&quot;ngRoute&quot;])" VSHIFT="26">
+<icon BUILTIN="full-4"/>
+</node>
+<node COLOR="#fd0000" CREATED="1478097299536" HGAP="48" ID="ID_1681711686" MODIFIED="1478098664510" TEXT="app.config([&apos;$routeProvider&apos;,function($routeProvider){ &#xa;&#x9;$routeProvider.when(&quot;/index&quot;,&#xa;                      {&#xa;                       templater:&quot;&quot;,(templateUrl=&quot;&#x8def;&#x5f84;&quot;)&#xa;                       controller:&quot;&quot; &#xa;&#x9; }) &#xa;&#xa; }])" VSHIFT="3">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1478098437065" ID="ID_283522429" MODIFIED="1478098445165" TEXT="&#x8fdb;&#x9636;">
+<node CREATED="1478098456922" ID="ID_76572150" MODIFIED="1478098468315" TEXT="&#x76ee;&#x6807;&#xff1a;&#x5b9e;&#x73b0;&#x5d4c;&#x5957;"/>
+<node CREATED="1478098470188" ID="ID_1235291072" MODIFIED="1478098470188" TEXT=""/>
+</node>
+</node>
+<node CREATED="1478098332911" ID="ID_235923327" MODIFIED="1478098868244" TEXT="ui.route">
+<node CREATED="1478098368426" FOLDED="true" ID="ID_1910556833" MODIFIED="1478098895270" TEXT="base">
+<node CREATED="1478096809011" HGAP="25" ID="ID_1039729787" MODIFIED="1478097289664" TEXT="ng-app=&quot;app&quot;" VSHIFT="-18">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1478096824182" FOLDED="true" ID="ID_1035080640" MODIFIED="1478097819134" TEXT="&#x5bfc;&#x5165;js&#x6587;&#x4ef6;">
+<icon BUILTIN="full-2"/>
+<node CREATED="1478096854438" HGAP="38" ID="ID_639621487" MODIFIED="1478096935910" TEXT="angular.js" VSHIFT="-11"/>
+<node CREATED="1478096879390" HGAP="32" ID="ID_260006991" MODIFIED="1478097796995" TEXT="ui.route.js" VSHIFT="4"/>
+</node>
+<node CREATED="1478098679612" HGAP="22" ID="ID_1627078359" MODIFIED="1478098892298" VSHIFT="20">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;div ui-view&gt;&lt;/div&gt;
+    </p>
+    <p>
+      &lt;a href=&quot;#/home&quot;&gt;&lt;/div&gt;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="clanbomber"/>
+</node>
+<node CREATED="1478097091336" HGAP="21" ID="ID_1264120633" MODIFIED="1478098712230" TEXT="var app = angular.module(&apos;app&apos;,[&quot;ui.router&quot;])" VSHIFT="26">
+<icon BUILTIN="full-4"/>
+</node>
+<node COLOR="#fd0000" CREATED="1478097834444" HGAP="21" ID="ID_1855159315" MODIFIED="1478098718910" TEXT="app.config([&quot;$stateProvider&quot;,function($stateProvider){&#xa;&#x9;$stateProvider.state(&quot;index&quot;,{&#xa;&#x9;        url:&quot;/index&quot;(&#x4e00;&#x5b9a;&#x8981;&#x659c;&#x6760;//////);&#xa;&#x9;        template:&quot;&quot;;(&#x5efa;&#x8bae;&#x7528;templateUrl=&quot;views/index&quot;);&#xa;&#x9;       controller:&quot;&quot;&#xa;&#x9;})&#xa;}])" VSHIFT="17">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
